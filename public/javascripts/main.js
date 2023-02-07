06d41f08940391c5c2e15bc42082ebe113c5d5fb
Alex Dzyuba <alexeydzyuba27@gmail.com>
2023-02-07 13:46:29 +0100

    remove render

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -9,11 +9,7 @@ likeButton.addEventListener('click', () => {
   likesCountElem.innerText = likesCount
 
   // Отправить запрос на сервер чтобы сервер у себя обновил количество лайков
-  let url = 'http://localhost:3000/likes/update'
-  if (window.isAppRunningOnRender) {
-    url = 'https://helpj-express.onrender.com/likes/update'
-  }
-  fetch(url, {
+  fetch('http://hj3exp/likes/update', {
     method: 'POST',
     body: likesCount
   })
--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,8 @@ const fs = require('fs')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   const currentLikesCount = fs.readFileSync('likesCount.txt').toString()
-  let isAppRunningOnRender = false
-  if (process.env.RUNNING_ON_RENDER) {
-    isAppRunningOnRender = true
-  }
 
-  res.render('index', { likesCount: currentLikesCount, isAppRunningOnRender });
+  res.render('index', { likesCount: currentLikesCount });
 });
 
 module.exports = router;
